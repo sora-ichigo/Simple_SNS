cf7368cfb4da2372d8eb1c750dd2eb2c04657495
ichigo <ichigo@sora.com>
2020-11-08 00:04:17 +0900

    fixed: auth周りを日本語にした

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
     }
 }
